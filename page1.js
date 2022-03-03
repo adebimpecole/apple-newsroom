c370507f6ed08c22d276a59d3cae4e9fc53980c8
adebimpecole <74488899+adebimpecole@users.noreply.github.com>
2022-03-03 17:13:54 +0100

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,9 @@
+(function(){
+    "use strict";
+    const nxt = document.getElementsByClassName("next");
+    const img = document.getElementsByClassName("slide");
+    sad.addEventListner("click", function(){
+        \
+    })
+     
+})();
